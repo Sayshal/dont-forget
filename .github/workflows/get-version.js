923219c3ac06a0ca5a29e841ce188a7950434d01
Tyler <3deathsaves@gmail.com>
2025-03-19 10:55:39 -0600

    workflow changes

--- .github/workflows/get-version.js
+++ .github/workflows/get-version.js
@@ -1,3 +1,7 @@
-// Thanks gambit (github.com/gambit07) for letting me test with his workflow!
-let fs = require('fs')
-console.log(JSON.parse(fs.readFileSync('./module.json', 'utf8')).version)
+let fs = require('fs');
+const moduleJson = JSON.parse(fs.readFileSync('./module.json', 'utf8'));
+
+console.log('VERSION:' + moduleJson.version);
+console.log('COMPAT_MIN:' + moduleJson.compatibility.minimum);
+console.log('COMPAT_VERIFIED:' + moduleJson.compatibility.verified);
+console.log('COMPAT_MAX:' + moduleJson.compatibility.maximum);
